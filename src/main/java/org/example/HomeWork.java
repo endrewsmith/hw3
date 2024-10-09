4f036a8820ca6301aefe4c9e9c2d1c3a2af953de
Andrew Kuznetsov <56806222+endrewsmith@users.noreply.github.com>
2024-10-09 11:03:22 +0300

    Update HomeWork.java

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -23,11 +23,78 @@ public class HomeWork {
      * @return максимальная подстрока из уникальных значений
      * Сигнатуру метода не меняем
      */
-    public String findMaxSubstring(String str) {
-        //TODO реализовать метод
-        return null;
+public String findMaxSubstring(String str) {
+
+        // Проверка на null
+        if (str == null) {
+            return "";
+        }
+        // Используем TreeMap, где ключ - размер строки, значение сама строка
+        TreeMap tree = new TreeMap<Integer, String>();
+        // Временная строка
+        String tmp = new String();
+        // Разобъем строку на массив символов для цикла
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            // Если символа нет в временной строке, то добавляем его в строку
+            if (tmp.indexOf(charArray[i]) < 0) {
+                tmp = tmp + charArray[i];
+                // Если символ есть, заканчиваем строку
+            } else {
+                // Если строка такой длины уже есть, не вносим ее в tree
+                tree.putIfAbsent(tmp.length(), tmp);
+                tmp = new String();
+                tmp = tmp + charArray[i];
+            }
+            // Проверка последней строки цикла перед закрытием
+            if (i == charArray.length - 1) {
+                tree.putIfAbsent(tmp.length(), tmp);
+            }
+        }
+
+        System.out.println(tree.get(tree.lastKey()));
+        return tree.get(tree.lastKey()).toString();
     }
 
+    // Без TreeMap только на строках
+    public String findMaxSubstringSimple(String str) {
+
+        // Проверка на null
+        if (str == null) {
+            return "";
+        }
+
+        int maxSize = 0;
+        String finalStr = new String();
+        // Временная строка
+        String tmp = new String();
+        // Разобъем строку на массив символов для цикла
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            // Если символа нет в временной строке, то добавляем его
+            if (tmp.indexOf(charArray[i]) < 0) {
+                tmp = tmp + charArray[i];
+                // Если символ есть, заканчиваем строку и проверяем ее размер
+            } else {
+                // Если размер больше текущего максимального, меняем финальную строку и макс. раземер
+                if (maxSize < tmp.length()) {
+                    maxSize = tmp.length();
+                    finalStr = tmp;
+                }
+                tmp = new String();
+                tmp = tmp + charArray[i];
+            }
+            // Проверка последней строки цикла перед закрытием
+            if (i == charArray.length - 1) {
+                if (maxSize < tmp.length()) {
+                    finalStr = tmp;
+                }
+            }
+        }
+
+        System.out.println(finalStr);
+        return finalStr;
+    }
 
     /**
      * Задача со зведочкой (опционально)
@@ -39,8 +106,40 @@ public class HomeWork {
      * Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
      * @see <a href="https://www.codewars.com/kata/545cedaa9943f7fe7b000048">https://www.codewars.com/kata/545cedaa9943f7fe7b000048</a>
      */
-    public boolean check(String sentence){
-        return false;
+    // Метод реализован для англ языка, можно параметризовать под выбор языка
+    public boolean check(String sentence) {
+
+        if(sentence == null || sentence.isEmpty()){
+            return false;
+        }
+        // Строки можно менять при параметризации языка
+        // строка алфавита в нижнем регистре
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        // regex для языка: [^а-яёА-ЯЁ] - рус,  [^a-zA-Z] - англ, [^a-zA-Zа-яёА-ЯЁ] - рус и анг
+        String regexAbc = "[^a-zA-Z]";
+
+        HashSet set = new HashSet();
+        char[] charArray = abc.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            set.add(charArray[i]);
+        }
+
+        // Оставляем в строке только англ и рус буквы в нижнем регистре, если их нет вернем false
+        sentence = sentence.replaceAll(regexAbc, "").toLowerCase();
+        if (sentence.isEmpty()){
+            return false;
+        }
+        // Оставшуюся строку разбиваем на символы в массив
+        char[] chArray = sentence.toCharArray();
+        for (int i = 0; i < chArray.length; i++) {
+            // удаляем букву из множества
+            set.remove(chArray[i]);
+        }
+        // Если не все буквы удалены, то строка не pangram
+        if (set.size() > 0) {
+            return false;
+        }
+        return true;
     }
 
 }
